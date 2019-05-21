23bc864507abc6d41adb3a5ab82dce35f3d8cd46
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-05-21 16:22:19 -0700

    issue-44-ng7-travis  -  Attempt to folow Google's recipe

--- projects/living-style-guide/karma.conf.travisci.js
+++ projects/living-style-guide/karma.conf.travisci.js
@@ -8,39 +8,34 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/living-style-guide'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    reporters: ['mocha'],
+    // reporter options
+    mochaReporter: {
+      colors: {
+        success: 'blue',
+        info: 'green',
+        warning: 'cyan',
+        error: 'red'
+      },
+      symbols: {
+        success: '+',
+        info: '#',
+        warning: '!',
+        error: 'x'
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['TravisCI_ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      TravisCI_ChromeHeadlessNoSandbox: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          '--disable-translate',
-          '--disable-extensions',
-            '--disable-dev-shm-usage',
-          '--no-sandbox',  // Added to fix an issue where of Failed to connect to chrome browser
-         '--remote-debugging-port=9222',
-        ],
-      },
-    },
-    singleRun: true,
-    restartOnFileChange: true
+    browsers: ['ChromeHeadless'],
+    concurrency: Infinity
   });
 };
