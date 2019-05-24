2a2a241305c685d0180696f977cb4d70f8897401
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-05-23 20:57:58 -0700

    issue-44-ng7-canvas  -  Set up the library component to grab the template, its parameters, and to render a simple rectangle on the template'; next is to add all parameters and the logic to render the actual spark bars.

--- projects/library/src/lib/spark-barchart/spark-barchart.component.ts
+++ projects/library/src/lib/spark-barchart/spark-barchart.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'rms-spark-barchart',
   templateUrl: './spark-barchart.component.html',
   styleUrls: ['./spark-barchart.component.css']
 })
-export class SparkBarchartComponent implements OnInit {
+export class SparkBarchartComponent implements AfterViewInit {
+  @Input('spark_color') color: string;
+
+  // see https://blog.angular-university.io/angular-viewchild/
+  // for a in-depth discussion on @ViewChild
+  @ViewChild('sparklineCanvas') sparklineCanvas: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  // see https://blog.angular-university.io/angular-viewchild/
+  // for recommendation to use ngAfterViewInit instead of ngOnInit
+  ngAfterViewInit() {
+      let ctx: CanvasRenderingContext2D = this.sparklineCanvas.nativeElement.getContext('2d');
 
+      // Draw
+      ctx.fillStyle = this.color;
+      ctx.fillRect(20, 20, 150, 100);
+  }
 }
