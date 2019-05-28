4a1a21eefa2de28011d0db98ac7be9b720a405d9
Rodrigo Silveira <r3850365@gmail.com>
2019-05-28 04:56:04 -0700

    issue-44-ng7-spark-line  -  Fixed tests so they work

--- projects/living-style-guide/src/app/inline/inline.component.spec.ts
+++ projects/living-style-guide/src/app/inline/inline.component.spec.ts
@@ -1,29 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { AngularMaterialModuleModule } from '../utils/angular-material-module/angular-material-module.module';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { InlineComponent } from './inline.component';
+import { HttpModule } from '@angular/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SparkLineSimpleComponent } from './spark-line-simple/spark-line-simple.component'
 import { LibraryModule } from 'library';
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
 
 describe('InlineComponent', () => {
-  let component: InlineComponent;
-  let fixture: ComponentFixture<InlineComponent>;
+    let component: InlineComponent;
+    let fixture: ComponentFixture<InlineComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ InlineComponent ],
-      imports: [
-        LibraryModule,
-      ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [
+            InlineComponent,
+            SparkLineSimpleComponent,
+        ],
+        imports: [
+            AngularMaterialModuleModule,
+            BrowserAnimationsModule,
+            LibraryModule,
+            HttpModule,
+        	HttpClientModule,
+            MarkdownModule.forRoot({
+        		loader: HttpClient, // optional, only if you use [src] attribute
+        		markedOptions: {
+        			provide: MarkedOptions,
+        			useValue: {
+        				gfm: true,
+        				tables: true,
+        				breaks: false,
+        				pedantic: false,
+        				sanitize: false,
+        				smartLists: true,
+        				smartypants: false,
+        			},
+        		},
+        	}),
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InlineComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        ]
+        }).compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InlineComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- projects/living-style-guide/src/app/inline/spark-line-simple/spark-line-simple.component.spec.ts
+++ projects/living-style-guide/src/app/inline/spark-line-simple/spark-line-simple.component.spec.ts
@@ -1,25 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SparkLineSimpleComponent } from './spark-line-simple.component';
+import { AngularMaterialModuleModule } from '../../utils/angular-material-module/angular-material-module.module';
+import { HttpModule } from '@angular/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SparkLineSimpleComponent } from './spark-line-simple.component'
+import { LibraryModule } from 'library';
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+
 
 describe('SparkLineSimpleComponent', () => {
-  let component: SparkLineSimpleComponent;
-  let fixture: ComponentFixture<SparkLineSimpleComponent>;
+    let component: SparkLineSimpleComponent;
+    let fixture: ComponentFixture<SparkLineSimpleComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SparkLineSimpleComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [
+            SparkLineSimpleComponent,
+        ],
+        imports: [
+            AngularMaterialModuleModule,
+            LibraryModule,
+            HttpModule,
+            HttpClientModule,
+            MarkdownModule.forRoot({
+                loader: HttpClient, // optional, only if you use [src] attribute
+                markedOptions: {
+                    provide: MarkedOptions,
+                    useValue: {
+                        gfm: true,
+                        tables: true,
+                        breaks: false,
+                        pedantic: false,
+                        sanitize: false,
+                        smartLists: true,
+                        smartypants: false,
+                    },
+                },
+            }),
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SparkLineSimpleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        ]
+        }).compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SparkLineSimpleComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
