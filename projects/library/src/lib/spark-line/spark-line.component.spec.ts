dc58fce24a22cdd08e8ef5268a4e7d7f4664ab78
Rodrigo Silveira <r3850365@gmail.com>
2019-05-28 07:47:17 -0700

    issue-44-ng7  -  Fixed the library tests

--- projects/library/src/lib/spark-line/spark-line.component.spec.ts
+++ projects/library/src/lib/spark-line/spark-line.component.spec.ts
@@ -16,6 +16,15 @@ describe('SparkLineComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SparkLineComponent);
     component = fixture.componentInstance;
+    component.className= '';
+    component.decorationPointsString = JSON.stringify([]);
+    component.dotRadius=1.5;
+    component.height=32;
+    component.lineColor='Black';
+    component.linePointsString = JSON.stringify([1, 2, 3]);
+    component.lineWidth=1.5;
+    component.shadeColor='';
+    component.width=128;
     fixture.detectChanges();
   });
 
