d01d3682d8428aeef43d32dceeba102bb95400db
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-25 08:35:22 -0700

    issue-44-ng7-sparklines  -  Update the public api

--- projects/sparklines/src/public-api.ts
+++ projects/sparklines/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/sparklines.service';
 export * from './lib/sparklines.component';
 export * from './lib/sparklines.module';
+export * from './lib/spark-barchart/spark-barchart.component';
+export * from './lib/spark-boxplot/spark-boxplot.component';
+export * from './lib/spark-line/spark-line.component';
