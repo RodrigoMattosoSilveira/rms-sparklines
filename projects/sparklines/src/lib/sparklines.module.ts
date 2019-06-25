0d01330d5f478357c0e36e6c457ff59c151ee681
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-25 08:32:43 -0700

    issue-44-ng7-sparklines  -  Added the module components

--- projects/sparklines/src/lib/sparklines.module.ts
+++ projects/sparklines/src/lib/sparklines.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { SparklinesComponent } from './sparklines.component';
+import { SparkBarchartComponent } from './spark-barchart/spark-barchart.component';
+import { SparkBoxplotComponent } from './spark-boxplot/spark-boxplot.component';
+import { SparkLineComponent } from './spark-line/spark-line.component';
 
 @NgModule({
-  declarations: [SparklinesComponent],
+  declarations: [
+     SparklinesComponent,
+     SparkBarchartComponent,
+     SparkBoxplotComponent,
+     SparkLineComponent,
+  ],
   imports: [
   ],
-  exports: [SparklinesComponent]
+  exports: [
+     SparklinesComponent,
+     SparkBarchartComponent,
+     SparkBoxplotComponent,
+     SparkLineComponent,
+  ]
 })
 export class SparklinesModule { }
