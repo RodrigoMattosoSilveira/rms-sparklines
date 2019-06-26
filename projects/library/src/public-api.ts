ea3a12d8ae84ab2c31d64a796804ddddeddd23d0
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-26 14:29:58 -0700

    issue-44-ng7-sparklines  -  Removed library

--- projects/library/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of library
- */
-
-export * from './lib/library.service';
-export * from './lib/library.module';
-export * from './lib/spark-barchart/spark-barchart.component';
-export * from './lib/spark-boxplot/spark-boxplot.component';
-export * from './lib/spark-line/spark-line.component';
