d3660d098e094e038a9675e552c54e57e9f4dc7b
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-07-03 13:14:33 -0700

    issue-30-bullet  -  Wrote a new CoordinateTooltip class

---
+++ projects/rmstek-sparklines/src/lib/services/coordinate-tip.ts
@@ -0,0 +1,23 @@
+import { Rectangle } from './rectangle';
+
+export class CoordinateTip {
+   rect: Rectangle;
+   color: string;
+   tip: string;
+
+   constructor(rect: Rectangle, color: string, tip: string) {
+      this.rect = rect;
+      this.color = color;
+      this.tip = tip;
+   }
+   getRect(): Rectangle { return this.rect; }
+   setRect(value: Rectangle): void { this.rect = value }
+
+   getColor(): string { return this.color; }
+   setColor(value: string): void { this.color = value }
+
+   getTip(): string { return this.tip; }
+   setTip(value: string): void { this.tip = value }
+
+   draw(ctx: CanvasRenderingContext2D) {}
+}
