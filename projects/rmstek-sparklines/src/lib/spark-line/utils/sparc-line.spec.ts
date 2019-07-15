46004d3463a8a7e1fe5a7f9d14e9ef20cfcf63ad
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-07-14 18:04:16 -0700

    issue-46-oop-spark-line  -  spark-line constructor works

--- projects/rmstek-sparklines/src/lib/spark-line/utils/sparc-line.spec.ts
+++ projects/rmstek-sparklines/src/lib/spark-line/utils/sparc-line.spec.ts
@@ -4,20 +4,47 @@ import { SparkLine } from './spark-Line';
 
 describe(`SparkLine`, () => {
 	var sparkLine: SparkLine;
+   var decorationPointsRaw: string;
+   var dotRadiusRaw: string;
+   var heightRaw: string;
+   var lineColorsRaw: string;
+   var linePointsRaw: string;
+   var lineWidthRaw: string;
+   var shadeColorRaw: string;
+   var widthRaw: string;
 
-	var comparativeMeasureRaw: string;
-   var featureMeasureRaw: string;
-   var height: number;
-	var heightRaw: string;
-	var qualitativeRangesRaw: string;
-   var width: number;
-	var widthRaw: string;
-
-	beforeEach(() => {
-		sparkLine = new SparkLine();
-	});
-
-   it(`should be created`, () => {
-      expect(sparkLine).toBeTruthy();
+   describe(`should be`, () => {
+      beforeEach(() => {
+         decorationPointsRaw = `decorationPointsRaw`;
+         dotRadiusRaw = `dotRadiusRaw`;
+         heightRaw = `heightRaw`;
+         lineColorsRaw = `lineColorsRaw`;
+         linePointsRaw = `linePointsRaw`;
+         lineWidthRaw = `lineWidthRaw`;
+         shadeColorRaw = `shadeColorRaw`;
+         widthRaw = `widthRaw`;
+   		sparkLine = new SparkLine(decorationPointsRaw,
+            dotRadiusRaw,
+            heightRaw,
+            lineColorsRaw,
+            linePointsRaw,
+            lineWidthRaw,
+            shadeColorRaw,
+            widthRaw
+          );
+   	});
+      it(`created`, () => {
+         expect(sparkLine).toBeTruthy();
+      });
+      it(`initialized correctlyh`, () => {
+         expect(sparkLine.getDecorationPointsRaw()).toBeTruthy(`decorationPointsRaw`);
+         expect(sparkLine.getDotRadiusRaw()).toBe(`dotRadiusRaw`);
+         expect(sparkLine.getHeightRaw()).toBe(`heightRaw`);
+         expect(sparkLine.getLineColorRaw()).toBe(`lineColorsRaw`);
+         expect(sparkLine.getLinePointsRaw()).toBe(`linePointsRaw`);
+         expect(sparkLine.getLineWidthRaw()).toBe(`lineWidthRaw`);
+         expect(sparkLine.getShadeColorRaw()).toBe(`shadeColorRaw`);
+         expect(sparkLine.getWidthRaw()).toBe(`widthRaw`);
+      });
    });
 });
--- projects/rmstek-sparklines/src/lib/spark-line/utils/spark-line.ts
+++ projects/rmstek-sparklines/src/lib/spark-line/utils/spark-line.ts
@@ -1,7 +1,49 @@
 import { SparklineInterface} from '../../utils/sparkline-interface'
 
 export class SparkLine implements SparklineInterface{
-   constructor() {}
+   private decorationPointsRaw: string;
+   getDecorationPointsRaw(): string { return this.decorationPointsRaw; }
+   setDecorationPointsRaw(value: string): void { this.decorationPointsRaw = value; }
+   private dotRadiusRaw: string;
+   getDotRadiusRaw(): string { return this.dotRadiusRaw; }
+   setDotRadiusRaw(value: string): void { this.dotRadiusRaw = value; }
+   private heightRaw: string;
+   getHeightRaw(): string { return this.heightRaw; }
+   setHeightRaw(value: string): void { this.heightRaw = value; }
+   private lineColorsRaw: string;
+   getLineColorRaw(): string { return this.lineColorsRaw; }
+   setLineColorRaw(value: string): void { this.lineColorsRaw = value; }
+   private linePointsRaw: string;
+   getLinePointsRaw(): string { return this.linePointsRaw; }
+   setLinePointsRaw(value: string): void { this.linePointsRaw = value; }
+   private lineWidthRaw: string;
+   getLineWidthRaw(): string { return this.lineWidthRaw; }
+   setLineWidthRaw(value: string): void { this.lineWidthRaw = value; }
+   private shadeColorRaw: string;
+   getShadeColorRaw(): string { return this.shadeColorRaw; }
+   setShadeColorRaw(value: string): void { this.shadeColorRaw = value; }
+   private widthRaw: string;
+   getWidthRaw(): string { return this.widthRaw; }
+   setWidthRaw(value: string): void { this.widthRaw = value; }
+
+   constructor(decorationPointsRaw: string,
+      dotRadiusRaw: string,
+      heightRaw: string,
+      lineColorRaw: string,
+      linePointsRaw: string,
+      lineWidthRaw: string,
+      shadeColorRaw: string,
+      widthRaw: string
+   ) {
+      this.setDecorationPointsRaw(decorationPointsRaw);
+      this.setDotRadiusRaw(dotRadiusRaw);
+      this.setHeightRaw(heightRaw);
+      this.setLineColorRaw(lineColorRaw);
+      this.setLinePointsRaw(linePointsRaw);
+      this.setLineWidthRaw(lineWidthRaw);
+      this.setShadeColorRaw(shadeColorRaw);
+      this.setWidthRaw(widthRaw);
+   }
 
    validate(): void {}
    prepare(canvasEl?: HTMLCanvasElement): void {}
